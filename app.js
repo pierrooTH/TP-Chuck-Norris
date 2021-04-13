a37f5333911b1145f6918cf853ed55ce43770ab3
pierrooTH <pierre.thouvenot1711@gmail.com>
2021-04-13 17:17:12 +0200

    Amélioration du générateur de citations
    Ajout de la font 'Poppins'
    Et un peu de responsive...

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-
-
-
-
 function randomColor() {
     let r = Math.floor(256*Math.random());
     let g = Math.floor(256*Math.random());
@@ -12,18 +8,16 @@ function randomColor() {
     author.style.color = "rgba(" + r + "," + g + "," + b + ",1)";
 } 
 
-
-
 let myButton = document.querySelector("#btn");
 let citations = [
     "Chuck Norris fait pleurer les oignons.",
     "Chuck Norris comprend Jean-Claude Van Damme.",
     "Chuck Norris comprend Jean-Claude Van Damme.",
     "Un jour, Chuck Norris a fait la blague « j'ai volé ton nez à Mickael Jackson.",
-    "Chuck Norris n'allume jamais la lumière, c'est l'obscurité qui s'éteint devant lui",
-    "Peter Parker a été mordu par une araignée, Clark Kent a été mordu par Chuck Norris",
+    "Chuck Norris n'allume jamais la lumière, c'est l'obscurité qui s'éteint devant lui.",
+    "Peter Parker a été mordu par une araignée, Clark Kent a été mordu par Chuck Norris.",
     "Wall Street n'est que la 3ème bourse au monde, il y a d'abord les deux de Chuck Norris.",
-    "Quand Chuck Norris a quitté le cocon familial, il a mis une tape sur l’épaule de son père et lui lança : « maintenant, c’est toi l’homme de la maison »",
+    "Quand Chuck Norris a quitté le cocon familial, il a mis une tape sur l’épaule de son père et lui lança : « maintenant, c’est toi l’homme de la maison.»",
     "Chuck Norris et Superman ont fait un bras de fer, le perdant devait mettre son slip par dessus son pantalon.",
     "La seule chose qui arrive à la cheville de Chuck Norris...c'est sa chaussette."
 ]
@@ -31,11 +25,9 @@ let citationsText = document.querySelector("#paragraph__citations");
 let author = document.querySelector(".author__citations");
 citationsText.innerHTML = citations[0];
 
-
 myButton.addEventListener("click", function() {
     let randomValue = citations[Math.floor(citations.length * Math.random())];
     citationsText.innerHTML = randomValue;
 
     randomColor();
-    
 })
